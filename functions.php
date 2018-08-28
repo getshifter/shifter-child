3a48febbe22e6a14132471b6ce91889e838ee1f3
Daniel Olson <emaildano@gmail.com>
2018-08-28 10:23:00 -0400

    first-commit

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+function child_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles' );
+ 
+ 
+//add new code here
